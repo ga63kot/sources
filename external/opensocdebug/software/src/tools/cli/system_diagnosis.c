81ee97012aa71346e1641e51e5aa3ff2eaacb05c
Tim Fritzmann <tim.fritzmann@tum.de>
2016-09-12 14:05:20 +0200

    add system diagnosis

--- external/opensocdebug/software/src/tools/cli/system_diagnosis.c
+++ external/opensocdebug/software/src/tools/cli/system_diagnosis.c
@@ -29,42 +29,36 @@
 #include <assert.h>
 
 int write_configreg(struct osd_context *ctx) {
-    // Get list of memories
-    uint16_t *memories;
-    size_t num_memories;
     int success = 1;
-    printf("My TEST\n");
-    osd_get_memories(ctx, &memories, &num_memories);
+    printf("CONFREG TEST\n");
 
     uint16_t temp_read;
-    osd_reg_write16(ctx, 3, 0x200, 0x2030);		// First Event LSB
-    osd_reg_write16(ctx, 3, 0x201, 0x0000);		// First Event MSB
-    osd_reg_write16(ctx, 3, 0x202, 0x8001);		// First Event valid and Event ID 1
+    osd_reg_write16(ctx, 5, 0x200, 0x2030);		// First Event LSB
+    osd_reg_write16(ctx, 5, 0x201, 0x0000);		// First Event MSB
+    osd_reg_write16(ctx, 5, 0x202, 0x8001);		// First Event valid and Event ID 1
 
-    osd_reg_write16(ctx, 3, 0x203, 0xd758);		// Second Event LSB
-    osd_reg_write16(ctx, 3, 0x204, 0x0000);		// Second Event MSB
-    osd_reg_write16(ctx, 3, 0x205, 0x8002);		// Second Event valid and Event ID 2
+//    osd_reg_write16(ctx, 5, 0x203, 0xa600);		// Second Event LSB
+    osd_reg_write16(ctx, 5, 0x203, 0x0000);		// Second Event LSB
+    osd_reg_write16(ctx, 5, 0x204, 0x0000);		// Second Event MSB
+    osd_reg_write16(ctx, 5, 0x205, 0x8002);		// Second Event valid and Event ID 2
 
-    osd_reg_write16(ctx, 3, 0x206, 0xd74c);		// Third Event LSB
-    osd_reg_write16(ctx, 3, 0x207, 0x0000);		// Third Event MSB
-    osd_reg_write16(ctx, 3, 0x208, 0x8003);		// Third Event valid and Event ID 3
+    osd_reg_write16(ctx, 5, 0x206, 0xd334);		// Third Event LSB
+    osd_reg_write16(ctx, 5, 0x207, 0x0000);		// Third Event MSB
+    osd_reg_write16(ctx, 5, 0x208, 0x8003);		// Third Event valid and Event ID 3
  
-    osd_reg_write16(ctx, 3, 0x209, 0xffff);		// Configuration GPR selection vector (LSB)
-    osd_reg_write16(ctx, 3, 0x20a, 0x0000);		// Configuration GPR selection vector (MSB)
-    osd_reg_write16(ctx, 3, 0x20b, 0x8fc1);		// Configuration First Event valid and Event ID 1
+    osd_reg_write16(ctx, 5, 0x209, 0x0000);		// Configuration GPR selection vector (LSB)
+    osd_reg_write16(ctx, 5, 0x20a, 0x0000);		// Configuration GPR selection vector (MSB)
+    osd_reg_write16(ctx, 5, 0x20b, 0x8001);		// Configuration First Event valid and Event ID 1
 
-    osd_reg_write16(ctx, 3, 0x20c, 0xffff);		// Configuration Second Event LSB
-    osd_reg_write16(ctx, 3, 0x20d, 0x0000);		// Configuration Second Event MSB
-    osd_reg_write16(ctx, 3, 0x20e, 0x8fc2);		// Configuration Second Event valid and Event ID 2
+    osd_reg_write16(ctx, 5, 0x20c, 0xffff);		// Configuration Second Event LSB
+    osd_reg_write16(ctx, 5, 0x20d, 0x0000);		// Configuration Second Event MSB
+    osd_reg_write16(ctx, 5, 0x20e, 0x8fc2);		// Configuration Second Event valid and Event ID 2
 
-    osd_reg_write16(ctx, 3, 0x20f, 0xffff);		// Configuration Third Event LSB
-    osd_reg_write16(ctx, 3, 0x210, 0x0000);		// Configuration Third Event MSB
-    osd_reg_write16(ctx, 3, 0x211, 0x8fc3);		// Configuration Third Event valid and Event ID 3
+    osd_reg_write16(ctx, 5, 0x20f, 0x000f);		// Configuration Third Event LSB
+    osd_reg_write16(ctx, 5, 0x210, 0x0000);		// Configuration Third Event MSB
+    osd_reg_write16(ctx, 5, 0x211, 0x80c3);		// Configuration Third Event valid and Event ID 3
 
-
-
-    osd_reg_read16(ctx, 3, 0x200, &temp_read);
-    
-    printf("Currently defined events: %d\n", temp_read);
+//    osd_reg_read16(ctx, 5, 0x200, &temp_read); 
+//    printf("Currently defined events: %d\n", temp_read);
     return success;
 }
